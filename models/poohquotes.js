ce3c5321b80eb1f541d32df438152230d21692fa
Stephen Doty <51100906+stephendoty826@users.noreply.github.com>
2024-01-04 14:39:17 -0600

    Update poohquotes.js

--- models/poohquotes.js
+++ models/poohquotes.js
@@ -2,108 +2,108 @@ const poohQuotes = {
     data: 
         [{
         quote: "You can't stay in your corner of the Forest waiting for others to come to you. You have to go to them sometimes.",
-        author: "winnie-the-pooh",
+        author: "Winnie the Pooh",
         tags: ['pooh', 'forest']
     },
     {
         quote: "Promise me you'll always remember: You're braver than you believe, stronger than you seem, and smarter than you think.",
-        author: "winnie-the-pooh",
+        author: "Winnie the Pooh",
         tags: ['pooh', 'promise']
     },
     {
         quote: "You never can tell with bees.",
-        author: "winnie-the-pooh",
+        author: "Winnie the Pooh",
         tags: ['pooh', 'bees']
     },
     {
         quote: "A hug is always the right size.",
-        author: "winnie-the-pooh",
+        author: "Winnie the Pooh",
         tags: ['pooh', 'friendship', 'love']
     },
     {
         quote: "After all, one can't complain. I have my friends.",
-        author: "eeyore",
+        author: "Eeyore",
         tags: ['eeyore', 'friendship']
     },
     {
         quote: "The things that make me different are the things that make me, me.",
-        author: "piglet",
+        author: "Piglet",
         tags: ['piglet', 'self']
     },
     {
         quote: "Sometimes the smallest things take up the most room in your heart.",
-        author: "winnie-the-pooh",
+        author: "Winnie the Pooh",
         tags: ['pooh', 'love', 'heart']
     },
     {
         quote: "If you live to be a hundred, I hope I live to be a hundred minus one day, so that I never have to live a day without you.",
-        author: "winnie-the-pooh",
+        author: "Winnie the Pooh",
         tags: ['pooh', 'bees', 'friendship']
     },
     {
         quote: "A little consideration, a little thought for others, makes all the difference.",
-        author: "eeyore",
+        author: "Eeyore",
         tags: ['eeyore', 'consideration', 'friendship']
     },
      {
         quote: "I'm so rumbly in my tumbly.",
-        author: "winnie-the-pooh",
+        author: "Winnie the Pooh",
         tags: ['pooh', 'hunger']
     },
     {
         quote: "I always get to where I'm going by walking away from where I have been.",
-        author: "winnie-the-pooh",
+        author: "Winnie the Pooh",
         tags: ['pooh', 'goals']
     },
     {
         quote: "If the string breaks, then we try another piece of string.",
-        "author": "owl",
+        "author": "Owl",
         tags: ['owl', 'goals', 'perseverance']
     },
     {
         quote: "If you live to be a hundred, I hope I live to be a hundred minus one day, so that I never have to live a day without you.",
-        author: "winnie-the-pooh",
+        author: "Winnie the Pooh",
         tags: ['pooh', 'bees', 'friendship']
     },
     {
         quote: "It isn't much good having anything exciting, if you can't share it with somebody.",
-        author: "winnie-the-pooh",
+        author: "Winnie the Pooh",
         tags: ['pooh', 'goals', 'friendship']
     },
     {
         quote: "It is more fun to talk with someone who doesn't use long, difficult words but rather short, easy words like, 'What about lunch?'",
-        author: "winnie-the-pooh",
+        author: "Winnie the Pooh",
         tags: ['pooh', 'bees', 'friendship']
     },
     {
         quote: "Rivers know this: there is no hurry. We shall get there some day.",
-        author: "winnie-the-pooh",
+        author: "Winnie the Pooh",
         tags: ['pooh', 'bees', 'friendship']
     },
     {
         quote: "Weeds are flowers, too, once you get to know them.",
-        author: "winnie-the-pooh",
+        author: "Winnie the Pooh",
         tags: ['pooh']
     },
     {
         quote: "A day without a friend is like a pot without a single drop of honey left inside.",
-        author: "winnie-the-pooh",
+        author: "Winnie the Pooh",
         tags: ['pooh', 'friendship']
     },
     {
         quote: "If the person you are talking to doesn't appear to be listening, be patient. It may simply be that he has a small piece of fluff in his ear.",
-        author: "winnie-the-pooh",
+        author: "Winnie the Pooh",
         tags: ['pooh']
     },
     {
         quote: "The things that make me different are the things that make me.",
-        author: "winnie-the-pooh",
+        author: "Winnie the Pooh",
         tags: ['pooh']
     }],
     kanga: {
         id: 2121,
         quote: "If the person you are talking to doesn't appear to be listening, be patient. It may simply be that he has a small piece of fluff in his ear.",
-        author: "winnie-the-pooh",
+        author: "Winnie the Pooh",
         tags: ['pooh']
     },
 }
